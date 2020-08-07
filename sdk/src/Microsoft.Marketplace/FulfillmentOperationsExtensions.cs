4e9b589eab6af74396954b91cb94c32b0aa6e068
Ercenk Keresteci <ercenk@ercenk.com>
2020-08-06 19:05:41 -0700

    New .NET SDK - PR into dev (#25)
    * Add .NET Core build action
    
    * Update README.md
    
    * Copy //Build client code
    
    * Change dotnet version
    
    * Adjust the workdir for build action
    
    * Remove test step from the worklow file
    
    * Adjust workflow again
    
    * Moving globaljson
    
    * Rename namespaces
    
    * Rename namespaces
    
    * Add test step
    
    * Wrong repo
    
    * Updated .NET SDK version
    
    * Bring generated SDK from OAS in
    
    * Change .NET version
    
    * add generated
    
    * Use new resource ID
    
    * Getting closer to SDK
    
    * add .net sdk minor version fix
    
    * Update README.md
    
    * Update dotnet-core.yml
    
    * Update dotnet-core.yml
    
    * addressed code review comments
    
    * Moved implementation to use AAD v2.0
    
    * Update workflow to kick off build on dev branch
    
    * Set to allow for dev and master
    
    Co-authored-by: Ercenk Keresteci <ercenk@microsoft.com>
    Co-authored-by: Scott Seely <scseely@microsoft.com>

---
+++ sdk/src/Microsoft.Marketplace/FulfillmentOperationsExtensions.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Marketplace.Models;
+
+namespace Microsoft.Marketplace
+{
+    public static partial class FulfillmentOperationsExtensions
+    {
+        /// <summary>
+        ///     List all of the subscriptions by iteratively calling the List on subsequent pages
+        /// </summary>
+        /// <param name="fulfillment">
+        ///     Type being extended
+        /// </param>
+        /// <param name="cancellationToken">
+        ///     Cancellation token
+        /// </param>
+        /// <returns></returns>
+        public static async Task<IEnumerable<Subscription>> ListAllSubscriptionsAsync(
+            this IFulfillmentOperations fulfillment, CancellationToken cancellationToken = default)
+        {
+            var subscriptions = new List<Subscription>();
+            var page = await fulfillment.ListSubscriptionsAsync(null, null, null, cancellationToken);
+
+            subscriptions.AddRange(page);
+            while (page.NextPageLink != default)
+            {
+                page = await fulfillment.ListSubscriptionsNextAsync(page.NextPageLink, null, null, cancellationToken);
+                subscriptions.AddRange(page);
+            }
+
+            return subscriptions;
+        }
+    }
+}
