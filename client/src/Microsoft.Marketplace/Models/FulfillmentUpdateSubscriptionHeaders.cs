13676a9d32708a43d343f66c249459b4ab60687d
Ercenk Keresteci <ercenk@ercenk.com>
2020-08-06 19:05:41 -0700

    New .NET SDK - PR into dev (#25)
    * Add .NET Core build action
    
    * Update README.md
    
    * Copy //Build client code
    
    * Change dotnet version
    
    * Adjust the workdir for build action
    
    * Remove test step from the worklow file
    
    * Adjust workflow again
    
    * Moving globaljson
    
    * Rename namespaces
    
    * Rename namespaces
    
    * Add test step
    
    * Wrong repo
    
    * Updated .NET SDK version
    
    * Bring generated SDK from OAS in
    
    * Change .NET version
    
    * add generated
    
    * Use new resource ID
    
    * Getting closer to SDK
    
    * add .net sdk minor version fix
    
    * Update README.md
    
    * Update dotnet-core.yml
    
    * Update dotnet-core.yml
    
    * addressed code review comments
    
    * Moved implementation to use AAD v2.0
    
    * Update workflow to kick off build on dev branch
    
    * Set to allow for dev and master
    
    Co-authored-by: Ercenk Keresteci <ercenk@microsoft.com>
    Co-authored-by: Scott Seely <scseely@microsoft.com>

---
+++ client/src/Microsoft.Marketplace/Models/FulfillmentUpdateSubscriptionHeaders.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+
+using System;
+using Newtonsoft.Json;
+
+namespace Microsoft.Marketplace.Models
+{
+    public partial class FulfillmentUpdateSubscriptionHeaders
+    {
+        [JsonIgnore]
+        public Guid OperationLocation
+        {
+            get
+            {
+                // Operation-Location header returns a URI like the followign
+                // https://marketplaceapi.microsoft.com/api/saas/subscriptions/4c49739f-217f-5d1b-5722-69278d4015c1/operations/749eb64f-4d7a-4f1d-9bcd-7629b97a2d38?api-version=2018-08-31
+
+                if (!Uri.TryCreate(OperationLocationUri, UriKind.Absolute, out var uri) || uri.Segments.Length != 7)
+                    throw new ApplicationException(
+                        $"Operation location received {OperationLocationUri} does not have 7 segments.");
+
+                if (Guid.TryParse(uri.Segments[6], out var operationId)) return operationId;
+
+                throw new ApplicationException($"Cannot parse the operation ID {uri.Segments[6]} as a  Guid");
+            }
+        }
+    }
+}
