bb2f1c9834fe6060aeaeee0d1124e1723907ca9b
Ercenk Keresteci <ercenk@ercenk.com>
2020-08-06 19:05:41 -0700

    New .NET SDK - PR into dev (#25)
    * Add .NET Core build action
    
    * Update README.md
    
    * Copy //Build client code
    
    * Change dotnet version
    
    * Adjust the workdir for build action
    
    * Remove test step from the worklow file
    
    * Adjust workflow again
    
    * Moving globaljson
    
    * Rename namespaces
    
    * Rename namespaces
    
    * Add test step
    
    * Wrong repo
    
    * Updated .NET SDK version
    
    * Bring generated SDK from OAS in
    
    * Change .NET version
    
    * add generated
    
    * Use new resource ID
    
    * Getting closer to SDK
    
    * add .net sdk minor version fix
    
    * Update README.md
    
    * Update dotnet-core.yml
    
    * Update dotnet-core.yml
    
    * addressed code review comments
    
    * Moved implementation to use AAD v2.0
    
    * Update workflow to kick off build on dev branch
    
    * Set to allow for dev and master
    
    Co-authored-by: Ercenk Keresteci <ercenk@microsoft.com>
    Co-authored-by: Scott Seely <scseely@microsoft.com>

---
+++ sdk/src/Microsoft.Marketplace/MarketplaceTokenProvider.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+
+
+using Microsoft.Identity.Client;
+using Microsoft.Rest;
+using System;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Marketplace
+{
+    internal abstract class MarketplaceTokenProvider : ITokenProvider
+    {
+        protected readonly Guid tenantId;
+        protected readonly Guid appId;
+        protected readonly string authenticationEndpoint;
+        protected readonly string marketplaceScope;
+
+        protected MarketplaceTokenProvider(Guid tenantId, Guid appId, string authenticationEndpoint, string marketplaceScope)
+        {
+            this.tenantId = tenantId;
+            this.appId = appId;
+            this.authenticationEndpoint = authenticationEndpoint;
+            this.marketplaceScope = marketplaceScope;
+        }
+        public async Task<AuthenticationHeaderValue> GetAuthenticationHeaderAsync(CancellationToken cancellationToken)
+        {
+            var builder = ConfidentialClientApplicationBuilder
+                .Create(this.appId.ToString())
+                .WithAuthority(this.authenticationEndpoint + this.tenantId);
+
+            var adApp = this.AddSecret(builder).Build();
+
+            var scopes = new [] { $"{this.marketplaceScope}/.default" };
+
+            var result = await adApp.AcquireTokenForClient(scopes).ExecuteAsync();
+            return new AuthenticationHeaderValue("Bearer", result.AccessToken);
+        }
+
+        protected abstract ConfidentialClientApplicationBuilder AddSecret(ConfidentialClientApplicationBuilder builder);
+    }
+}
