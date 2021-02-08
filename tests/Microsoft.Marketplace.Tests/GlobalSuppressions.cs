867193c745aac8b15c5ff5ab7d6ee44a7257fcc1
Ercenk Keresteci <ercenk@ercenk.com>
2021-02-08 14:31:34 -0800

    Preview 2 update (#38)
    * New .NET SDK - PR into dev (#25)
    
    * Add .NET Core build action
    
    * Update README.md
    
    * Copy //Build client code
    
    * Change dotnet version
    
    * Adjust the workdir for build action
    
    * Remove test step from the worklow file
    
    * Adjust workflow again
    
    * Moving globaljson
    
    * Rename namespaces
    
    * Rename namespaces
    
    * Add test step
    
    * Wrong repo
    
    * Updated .NET SDK version
    
    * Bring generated SDK from OAS in
    
    * Change .NET version
    
    * add generated
    
    * Use new resource ID
    
    * Getting closer to SDK
    
    * add .net sdk minor version fix
    
    * Update README.md
    
    * Update dotnet-core.yml
    
    * Update dotnet-core.yml
    
    * addressed code review comments
    
    * Moved implementation to use AAD v2.0
    
    * Update workflow to kick off build on dev branch
    
    * Set to allow for dev and master
    
    Co-authored-by: Ercenk Keresteci <ercenk@microsoft.com>
    Co-authored-by: Scott Seely <scseely@microsoft.com>
    
    * Updated the code generation and split the clients (#27)
    
    * read-tree for Azure.Core.TestFramework
    
    * Checkpoint
    
    * checkpoint 2
    
    * Split OAS doc
    
    * Add extensions after OAS split
    
    * Clean autorest.md
    
    * updated NuGet packages
    
    * Merge into one assembly (#29)
    
    * Merge into one assembly
    
    * Added test instructions
    
    * FIx typo
    
    * Modified code generation logic, and refreshed SDK for resourceUri in metering. (#33)
    
    * preview2 - breaking changes with Autorest.CSharp V3
    
    Start using Azure.Core.TestFramework for recording
    
    Remove global.json
    
    Cosmetic
    
    Update test docs
    
    * preview2 - breaking changes with Autorest.CSharp V3
    
    Start using Azure.Core.TestFramework for recording
    
    Remove global.json
    
    Cosmetic
    
    Update test docs
    
    Modify build task to use msbuild
    
    * fix target frameworks
    
    * preview2 - breaking changes with Autorest.CSharp V3
    
    Start using Azure.Core.TestFramework for recording
    
    Remove global.json
    
    Cosmetic
    
    Update test docs
    
    Modify build task to use msbuild
    
    preview2 - breaking changes with Autorest.CSharp V3
    
    Start using Azure.Core.TestFramework for recording
    
    Remove global.json
    
    Cosmetic
    
    Update test docs
    
    fix target frameworks
    
    * Remove Directory.Build.* into csproj
    
    * Reintroduce Directory.Build.props
    
    * Add .net test explorer setting
    
    * preview2 - breaking changes with Autorest.CSharp V3
    
    Start using Azure.Core.TestFramework for recording
    
    Remove global.json
    
    Cosmetic
    
    Update test docs
    
    Modify build task to use msbuild
    
    preview2 - breaking changes with Autorest.CSharp V3
    
    Start using Azure.Core.TestFramework for recording
    
    Remove global.json
    
    Cosmetic
    
    Update test docs
    
    fix target frameworks
    
    preview2 - breaking changes with Autorest.CSharp V3
    
    Start using Azure.Core.TestFramework for recording
    
    Remove global.json
    
    Cosmetic
    
    Update test docs
    
    fix target frameworks
    
    Remove Directory.Build.* into csproj
    
    Reintroduce Directory.Build.props
    
    Add .net test explorer setting
    
    * Rehash usings
    
    * New test recordings and supporting code modification.
    
    * Modify github actions
    
    * Change test working directory
    
    * kick off workflow
    
    * Revert "kick off workflow"
    
    This reverts commit 7001847da487b3a818dc8deed599c2a1aeb2b5f1.
    
    * preview2 - breaking changes with Autorest.CSharp V3
    
    * add recordings
    
    * preview2 - breaking changes with Autorest.CSharp V3
    
    * preview2 - breaking changes with Autorest.CSharp V3
    
    * Preview 2: Breaking changes for the clients
    
    * Fixes for PR comments
    
    * Build settings reorganized for source file reuse
    
    * Generate code based on the latest APi changes
    
    * Generate code based on the latest API changes
    
    * Modify github action tosupport .NET 5.0
    
    * GiitHub actions target .NET framework
    
    * Support .NET 5 with github actions
    
    * Add .NET 5.0.* for github actions
    
    * Modify github action tosupport .NET 5.0
    
    Co-authored-by: Ercenk Keresteci <ercenk@microsoft.com>
    Co-authored-by: Scott Seely <scseely@microsoft.com>

---
+++ tests/Microsoft.Marketplace.Tests/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives should be placed correctly", Justification = "By design")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Not using in tests", Scope = "type", Target = "~T:Microsoft.Marketplace.Tests.FulfillmentTests")]
