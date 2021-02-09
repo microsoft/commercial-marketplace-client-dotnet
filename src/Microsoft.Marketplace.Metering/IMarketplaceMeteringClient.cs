017f8fb17feba36bcd346b90a80a3f3809c0aa5a
Ercenk Keresteci <ercenk@ercenk.com>
2021-02-09 14:10:41 -0800

    Clean dev branch with preview2 changes

---
+++ src/Microsoft.Marketplace.Metering/IMarketplaceMeteringClient.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Marketplace.Metering
+{
+    /// <summary> Metering client. </summary>
+    public interface IMarketplaceMeteringClient
+    {
+        /// <summary> Gets an instance of MeteringOperations. </summary>
+        MeteringOperations Metering { get; }
+    }
+}
